4446109d4ee719d6cdd4dd58c7cc115a10b0ffc9
pointbazaar <alex23667@gmail.com>
2018-11-24 02:02:50 +0100

    figure out how to get parameters

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
+
+#https://mycroft.ai/documentation/skills/introduction-developing-skills/
 
 
 class Vrr(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+        
+    @intent_file_handler('how.intent')
+    def handle_query(self,message):
+        a = message.data.get("a")
+        b = message.data.get("b")
+        self.speak(' say public transit from '+a+' to '+b)
+        
 
     @intent_file_handler('vrr.intent')
     def handle_vrr(self, message):
